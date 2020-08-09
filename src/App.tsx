05e82c97d167a7661aa4bb3577978d57265fb104
nonoakij <nonoakij@gmail.com>
2020-08-09 17:38:37 +0900

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Top from './pages/Top';
+import SignIn from './pages/SignIn';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
+export const MyContext = React.createContext({ isSignIn: false });
+
+const App: React.FC = () => {
+  const [isSignIn, setIsSignIn] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MyContext.Provider value={{ ...{ isSignIn, setIsSignIn } }}>
+      <Router>
+        <Header />
+        <Route exact path="/" component={Top} />
+        <Route path="/sign-in" component={SignIn} />
+      </Router>
+    </MyContext.Provider>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Top/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Top: React.FC = () => {
+  return <>TopPage</>;
+};
+
+export default Top;
